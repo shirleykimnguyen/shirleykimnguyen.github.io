193326890eebcc6f495fdd02ffc1677e0c3b464a
Hoa Nguyen <hoaannguyen07@gmail.com>
2022-01-10 23:53:31 -0600

    finalized hamburger menu, about, and working on footer

--- javascript/hamburgerMenu.js
+++ javascript/hamburgerMenu.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const hamburger = document.querySelector("#hamburger");
 const hamburger_check = document.querySelector("#hamburger_check");
 const mobileNavMenu = document.querySelector(".mobile_nav_menu");
@@ -11,9 +12,11 @@ function toggleMenu() {
     if (hamburger_check.checked) {
         mobileNavMenu.classList.remove("show_mobile_nav_menu");
         hamburger_check.checked = false;
+        document.body.style.overflow = "scroll";
     } else {
         mobileNavMenu.classList.add("show_mobile_nav_menu");
         hamburger_check.checked = true;
+        document.body.style.overflow = "hidden";
     }
 }
 
